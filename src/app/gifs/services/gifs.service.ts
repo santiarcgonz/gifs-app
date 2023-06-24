5d4ad8b10ac05483706318adcf8881986fe7da51
santiarcgonz <arcilasantiago@gmail.com>
2023-06-24 00:16:13 -0500

    Ya busca imagenes y las muestra

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-    private _tagsHistory: string[] = [];
+    public gifList: Gif[] = [];
 
-    constructor() { }
+    private _tagsHistory: string[] = [];
+    private apiKey: string = 'qAM6eHs07ZuLj32evZ1LTmTYYvi7i2wM';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+    constructor( private http: HttpClient) { }
     
     get tagsHistory(){
         return [...this._tagsHistory];
     }
 
-    searchTag( tag: string): void{
-
+    private organizeHistory(tag: string){
+        tag = tag.toLowerCase();
+        if(this._tagsHistory.includes(tag)){
+            this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag );
+        }
         this._tagsHistory.unshift(tag);
-        console.log('tags service',this.tagsHistory)
+        this._tagsHistory = this._tagsHistory.splice(0,10);
+    }
+
+    searchTag( tag: string): void{
+        if( tag.length === 0 ) return;
+        this.organizeHistory(tag);
+
+        const params = new HttpParams()
+            .set('api_key', this.apiKey)
+            .set('limit', '10')
+            .set('q', tag)
+
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+            .subscribe(resp => {
+                this.gifList = resp.data;
+            }
+
+        )
+        
     }
 }
